cb114ffb7d6a8372035305304279339a55859c94
BillMaN89 <billman-synas@hotmail.com>
2025-02-10 18:04:48 +0200

    finish addBookToLibrary function, with an error displa

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const myLibrary = [];
 
 function Book(title, author, pages, have_read = false){
-            this.title = title;
-            this.author = author;
-            this.pages = pages;
-            this.have_read = have_read;
-            this.info = function() {
-                return `Title : ${title} , by : ${author}, ${pages} pages long,
-                 Have I read it : ${have_read} `
-            }
-        }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.have_read = have_read;
+}
 
-function addBookToLibrary() {
-    
+const message =  document.querySelector(".message-container");
+message.setAttribute("style","color: red; font-size: 30px;")
+
+
+
+function addBookToLibrary(title,author,pages,have_read = false) {
+    //validate inputs
+    if (!title || typeof title !== 'string'){
+        message.textContent = "Please enter a valid book title!"
+        console.log("Invalid title");
+        return;
+    }
+    if (!author || typeof author !== 'string'){
+        message.textContent = "Please enter a valid author name!"
+        console.log("Invalid author");
+        return;
+    }
+    if (!pages || typeof pages !=='number' || pages <= 0){
+        message.textContent = "Please enter a valid number of pages!"
+        console.log("Invalid pages");
+        return;
+    }
+    //book creation
+    const book = new Book(title,author,pages,have_read);
+    myLibrary.push(book);
+    //log
+    console.log(`Book added successfully: Title = ${title}, Author = ${author}, Pages = ${pages}, Read = ${have_read}`);
 }
